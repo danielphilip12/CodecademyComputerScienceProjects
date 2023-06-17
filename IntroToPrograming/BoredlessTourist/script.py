1f17321edfd0d4ec51fa2b9c6c26a6022ef9dd9e
Daniel Gallo <daniel.gallo12@gmail.com>
2023-06-17 10:13:36 -0700

    Created attractions and functionality for adding new attractions

--- IntroToPrograming/BoredlessTourist/script.py
+++ IntroToPrograming/BoredlessTourist/script.py
@@ -1,8 +1,11 @@
 # Global Variables
 
-destinations = ["Paris, France", "Shanghai, China", "Los Angelos, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China",
+                "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
 test_tourist = ["Erin Wilkes", "Shanghai, China", ['historical site', 'art']]
+
+attractions = [[], [], [], [], []]
 
 
 # Functions
@@ -10,17 +13,35 @@ def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index
 
+
 def get_traveler_location(traveler):
     traveler_destination = traveler[1]
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 
+def add_attraction(destination, attraction):
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index]
+    attractions_for_destination.append(attraction)
+    return
 
 
 # Function Tests
-# print(get_destination_index("Los Angelos, USA"))
+# print(get_destination_index("Los Angeles, USA"))
 # print(get_destination_index("Paris, France"))
 # print(get_destination_index("Hyderabad, India"))
+# test_destination_index = get_traveler_location(test_tourist)
+# print(test_destination_index)
 
-test_destination_index = get_traveler_location(test_tourist)
-print(test_destination_index)
+add_attraction("Los Angeles, USA", ["Venice Beach", ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+print(attractions)
