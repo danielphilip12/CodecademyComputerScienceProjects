db55c0f86a4e8880bf262aca6d51f74c0cacd506
Daniel Gallo <daniel.gallo12@gmail.com>
2023-06-17 10:02:10 -0700

    Added test objects

---
+++ IntroToPrograming/BoredlessTourist/script.py
@@ -0,0 +1,3 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angelos, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_tourist = ["Erin Wilkes", "Shanghai, China", ['historical site', 'art']]
