b67c52b55c4853058f207ca47e8415635ba85a67
Daniel Gallo <daniel.gallo12@gmail.com>
2023-06-17 10:21:03 -0700

    Added interest finder logic

--- IntroToPrograming/BoredlessTourist/script.py
+++ IntroToPrograming/BoredlessTourist/script.py
@@ -25,6 +25,18 @@ def add_attraction(destination, attraction):
     attractions_for_destination.append(attraction)
     return
 
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for possible_attraction in attractions_in_city:
+        attraction_tags = possible_attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+
+    return attractions_with_interest
+
 
 # Function Tests
 # print(get_destination_index("Los Angeles, USA"))
@@ -45,3 +57,6 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 print(attractions)
+print("")
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
