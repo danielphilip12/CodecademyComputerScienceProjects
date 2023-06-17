1d1ee3ca01cddc47d72d58904666db48888b85da
Daniel Gallo <daniel.gallo12@gmail.com>
2023-06-17 10:28:21 -0700

    Added function to generate message for traveler and presnet attractions they might be interested in

--- IntroToPrograming/BoredlessTourist/script.py
+++ IntroToPrograming/BoredlessTourist/script.py
@@ -19,11 +19,13 @@ def get_traveler_location(traveler):
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 
+
 def add_attraction(destination, attraction):
     destination_index = get_destination_index(destination)
     attractions_for_destination = attractions[destination_index]
     attractions_for_destination.append(attraction)
     return
+
 
 def find_attractions(destination, interests):
     destination_index = get_destination_index(destination)
@@ -38,6 +40,21 @@ def find_attractions(destination, interests):
     return attractions_with_interest
 
 
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(
+        traveler_destination, traveler_interests)
+    interests_string = "Hi " + \
+        traveler[0] + ", we think you'll like these places around " + \
+        traveler_destination + ": "
+
+    for attraction in traveler_attractions:
+        interests_string += attraction + ", "
+
+    return interests_string
+
+
 # Function Tests
 # print(get_destination_index("Los Angeles, USA"))
 # print(get_destination_index("Paris, France"))
@@ -47,16 +64,26 @@ def find_attractions(destination, interests):
 
 add_attraction("Los Angeles, USA", ["Venice Beach", ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
-add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Paris, France", ["Arc de Triomphe", [
+               "historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
 add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
-add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", [
+               "skyscraper", "viewing deck"]])
 add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
 add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
-add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", [
+               "monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 print(attractions)
 print("")
 la_arts = find_attractions("Los Angeles, USA", ['art'])
 print(la_arts)
+
+print("")
+print("")
+
+smills_france = get_attractions_for_traveler(
+    ['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
