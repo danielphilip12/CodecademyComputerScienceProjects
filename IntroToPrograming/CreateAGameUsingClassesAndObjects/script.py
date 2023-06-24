18f6cf2c1046756939ea84a320cddbc549e46f1b
Daniel Gallo <daniel.gallo12@gmail.com>
2023-06-24 09:53:19 -0700

    Removed some lines

--- IntroToPrograming/CreateAGameUsingClassesAndObjects/script.py
+++ IntroToPrograming/CreateAGameUsingClassesAndObjects/script.py
@@ -1,11 +1,3 @@
 from Student import Student
 from Teacher import Teacher
 
-daniel = Student("Daniel", 17, 11)
-noah = Student("Noah", 14, 8)
-anna = Student("Anna", 17, 11)
-
-teacher = Teacher("Kara Gallo", "Ms.", "Chemistry", "Computer Science", "Physics")
-teacher1 = Teacher("Angela Hobson", "Mrs.", "English", "Chinese")
-
-print(teacher)
