11c70a2e5036f5c577275d193261949b357429bc
Daniel Gallo <daniel.gallo12@gmail.com>
2023-06-24 21:39:49 -0700

    Completed rock paper scissors game

---
+++ PortfolioProjects/PythonTerminalGame/script.py
@@ -0,0 +1,55 @@
+import random
+
+
+class RockPaperScissors:
+    def __init__(self):
+        self.choices = ["rock", "paper", "scissors"]
+
+    def player_choice(self):
+        while True:
+            player_choice = input("Choose rock, paper, or scissors: ")
+            if player_choice in self.choices:
+                break
+            print("That option is not available. Spelling does matter")
+
+        return player_choice
+
+    def computer_choice(self):
+        return self.choices[random.randint(0, len(self.choices)-1)]
+    
+    def determine_winner_or_tie(self, player, computer):
+        if player == computer:
+            return "It's a tie"
+
+        if player == "rock":
+            if computer == "scissors":
+                return "You win"
+            else:
+                return "You lose"
+        if player == "paper":
+            if computer == "rock":
+                return "You win"
+            else:
+                return "You lose"
+        if player == "scissors":
+            if computer == "paper":
+                return "You win"
+            else:
+                return "You lose"
+    
+    def play_game(self):
+        player = self.player_choice()
+        computer = self.computer_choice()
+
+        result = self.determine_winner_or_tie(player, computer)
+        print("You picked " + player)
+        print("Computer picked " + computer)
+        print(result)
+
+
+
+test = RockPaperScissors()
+# print(test.player_choice())
+# print(test.computer_choice())
+
+test.play_game()
